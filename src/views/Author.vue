9833c9d3f608d7fa1896fff5a57441280a89732a
Jake Steele <jacob.steele@ortec.com>
2021-09-23 14:57:37 -0400

    Fixed rendering

--- src/views/Author.vue
+++ src/views/Author.vue
@@ -1,32 +1,41 @@
 <template>
-<div id="author-panel">
-  <input v-model="email"/>
-  <ul>
-    <li v-for="post in posts" :key="post.slug">
-      {{post.title}}
-    </li>
-  </ul>
-</div>
+  <div>
+        <b-sidebar id="sidebar-no-header" aria-labelledby="sidebar-no-header-title" visible no-header shadow no-close-on-esc no-header-close>
+      <template>
+        <div class="p-3">
+          <h4 id="sidebar-no-header-title">Posts</h4>
+          <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item v-for="(post, index) in posts" 
+              :key="post.slug" active @click="selectPost(index)">{{post.title}}</b-nav-item>
+            </b-nav>
+          </nav>
+        </div>
+      </template>
+    </b-sidebar>
+    <div>
+      <h1>{{selectedPost.title}}</h1>
+  
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios"
 export default {
-  name: 'Author',
-  data() {
+  data: () => {
     return {
-      posts: []
-    }
-},
-  mounted(){
-   axios.get("/blog/posts/").then(response => response.json()).then(data => this.posts = data)
+      posts:[],
+      selectedPost: {}
+    };
   },
-  methods:{
-    getPosts(){
-      axios.get("/blog/posts/").then(response => {
-        console.log(response.data)
-        this.posts = response.data
-      })
+  async mounted(){
+    this.posts = await axios.get("/blog/posts/").then(response => response.data)
+  },
+  methods: {
+    selectPost(index){
+      this.selectedPost = this.posts[index];
     }
   }
-}
+};
+</script>
