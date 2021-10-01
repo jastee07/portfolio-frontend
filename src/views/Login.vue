2e1f7e510a29b0ad07e7a3bd6c0cae507e57507b
Jake Steele <jake.steele54@gmail.com>
2021-09-30 22:47:41 -0400

    Added error messages to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,9 +4,12 @@
     <form @submit.prevent="login">
       <input v-model="email" placeholder="email" />
       <br />
+      <span v-if="errors.email">{{errors.email[0]}}</span>
       <br />
       <input v-model="password" placeholder="password" type="password" />
       <br />
+      <span v-if="errors.non_field_errors">{{errors.non_field_errors[0]}}</span>
+      <span v-else-if="errors.password">{{errors.password[0]}}</span>
       <br />
       <button type="submit">Login</button>
     </form>
@@ -18,12 +21,18 @@ export default {
     return {
       email: "",
       password: "",
+      errors: {}
     };
   },
   methods: {
     async login(e) {
         e.preventDefault();
-      this.$store.dispatch('login', {email:this.email, password:this.password})
+      let errors = await this.$store.dispatch('login', {email:this.email, password:this.password})
+      if(errors){
+        this.errors = errors;
+      } else {
+        this.errors = {}
+      }
     },
   }
 };
